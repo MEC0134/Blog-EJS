4a3f053903051b2cf7d644eeea8520a9b6c67fec
M. Enes Celik <m96celik@gmail.com>
2023-06-18 16:36:06 -0700

    optimization & cookies

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-var _ = require('lodash');
+const _ = require('lodash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,65 +16,79 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
+app.use(session({
+  secret: 'abcd1234!',
+  resave: true,
+  saveUninitialized: true
+}));
 
-// store posts 
+
 let postsArr = [];
 
 
-// home route
 app.get('/', (req, res) => {
+
+  if (!req.cookies.visited) {
+    res.cookie('visited', 'true');
+
+    
   res.render("home", {
     startingContent: homeStartingContent,
-    posts: postsArr, 
-    _: _
+    posts: postsArr,
+    _ : _,
+    showGuide: true 
   });
+  } else {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: postsArr,
+      _ : _,
+      showGuide: false 
+    });
+  }
+
 });
 
 
-// about route
 app.get('/about', (req, res) => {
   res.render("about", { aboutInfo: aboutContent });
 });
 
-// contact route
 app.get('/contact', (req, res) => {
   res.render("contact", { contactInfo: contactContent });
 });
 
 
-// compose route
 app.get('/compose', (req, res) => {
   res.render("compose")
 })
 
 
-// compose post 
 app.post('/compose', (req, res) => {
-
   const post = {
     title: req.body.postTitle,
     content: req.body.postText
   };
-
   postsArr.push(post);
-
   res.redirect('/');
 });
 
 
 app.get('/post/:postName', (req, res) => {
-
   const reqTitle = req.params.postName;
-
-  for (let i = 0; i < postsArr.length; i++) {
-
-    if (_.kebabCase(postsArr[i].title) === reqTitle) {
-
-      res.render("post", { postTitle: postsArr[i].title, postContent: postsArr[i].content});
-    }
+  const foundPost = postsArr.find(post => _.kebabCase(post.title) === reqTitle);
+  if (foundPost) {
+    res.render("post", { postTitle: foundPost.title, postContent: foundPost.content });
   }
-
 });
+
+
+
+
+
+
+
 
 
 
